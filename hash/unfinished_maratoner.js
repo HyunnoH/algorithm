83124ba4686239c32ec61d6e2296a3773cd3e455
Hyun Oh <hyunnoh01@gmail.com>
2021-04-07 22:02:42 +0900

    Create unfinished_maratoner.js

---
+++ hash/unfinished_maratoner.js
@@ -0,0 +1,10 @@
+function solution(participant, completion) {
+    participant.sort();
+    completion.sort();
+    
+    for (const i in participant) {
+        if(participant[i] !== completion[i]) {
+            return participant[i];
+        }
+    }
+}
